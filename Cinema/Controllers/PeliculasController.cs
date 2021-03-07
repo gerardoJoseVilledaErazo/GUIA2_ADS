b17f41cab4d390fc97b86a8bf91c7ae1566f7c4d
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2021-03-07 13:24:33 -0600

    version 1.01

---
+++ Cinema/Controllers/PeliculasController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cinema.Models;
+
+namespace Cinema.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        // GET: Peliculas/Aleatorias
+        public ActionResult Aleatorias()
+        {
+            Pelicula pelicula1 = new Pelicula() {Id = 1, Nombre = "Shrek", Genero = "Animacion, Aventura, Comedia", AnioLanzamiento = "2001", Clasificacion = "PG" };
+
+            //pelicula.nombre = "Shrek"; Otra forma en que se puede añadir
+
+            return View(pelicula1);
+        }
+    }
+}
